6ec4e197886f54af2002340043fbd73aa89c9559
ithabet <info@ovisioneg.com>
2022-04-07 06:14:33 +0200

    redirect

--- packages/Ithabet/Xlstart/src/views/layouts/main.blade.php
+++ packages/Ithabet/Xlstart/src/views/layouts/main.blade.php
@@ -110,8 +110,7 @@ use this class instead of -- horizontal-layout horizontal-menu
 
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-left d-inline-block">2021 &copy; Ovision</span><span class="float-right d-sm-inline-block d-none">Crafted with<i class="bx bxs-heart pink mx-50 font-small-3"></i>by<a class="text-uppercase" href="https://ovisioneg.com" target="_blank">Ovision</a></span>
-        <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="bx bx-up-arrow-alt"></i></button>
+    <p class="clearfix mb-0"><span class="float-left d-inline-block">2021 &copy; MAPP</span><span class="float-right d-sm-inline-block d-none">Crafted with<i class="bx bxs-heart pink mx-50 font-small-3"></i> by <a class="text-uppercase" href="#" target="_blank">MAPP</a></span>
     </p>
 </footer>
 <!-- END: Footer-->
