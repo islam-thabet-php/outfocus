be150777f4311f050c9c16f5d37105774c9bc09c
ithabet <info@ovisioneg.com>
2022-04-07 03:30:09 +0200

    vendor

---
+++ vendor/bnbwebexpertise/laravel-attachments/src/Http/Controllers/DownloadController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bnb\Laravel\Attachments\Http\Controllers;
+
+use Bnb\Laravel\Attachments\Contracts\AttachmentContract;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Lang;
+
+class DownloadController extends Controller
+{
+
+    /**
+     * Attachment model
+     *
+     * @var AttachmentContract
+     */
+    protected $model;
+
+
+    public function __construct(AttachmentContract $model)
+    {
+        $this->model = $model;
+    }
+
+
+    public function download($id, Request $request)
+    {
+        $disposition = ($disposition = $request->input('disposition')) === 'inline' ? $disposition : 'attachment';
+
+        if ($file = $this->model->where('uuid', $id)->first()) {
+            try {
+                /** @var \Bnb\Laravel\Attachments\Attachment $file */
+                if ( ! $file->output($disposition)) {
+                    abort(403, Lang::get('attachments::messages.errors.access_denied'));
+                }
+            } catch (FileNotFoundException $e) {
+                abort(404, Lang::get('attachments::messages.errors.file_not_found'));
+            }
+        }
+
+        abort(404, Lang::get('attachments::messages.errors.file_not_found'));
+    }
+}
